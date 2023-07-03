59873945a9c8ec24ffb7f44c3feebf9cc0a9598a
andishe abbasian <rainbowboxdash@gmail.com>
2023-07-03 20:10:48 +0330

    deleted

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,23 +1,17 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
-import usePlatform from "../hooks/usePlatform";
-import usePlatforms from "../hooks/usePlatforms";
 import useGameQueryStore from "../store";
 import { useQuery } from "@apollo/client";
 import { GET_LOCATION } from "../services/apolloClient";
 const PlatformSelector = () => {
-  const { data, error } = usePlatforms();
   const setSelectedPlatformId = useGameQueryStore((s) => s.setPlatformId);
-  const selectedPlatformId = useGameQueryStore((s) => s.gameQuery.platformId);
-  const selectedPlatform = usePlatform(selectedPlatformId);
   const { loading, error: asd, data: asdasd } = useQuery(GET_LOCATION);
-
-  if (error) return null;
 
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-        {selectedPlatform?.name || "Locations"}
+        {/* {selectedPlatform?.name || "Locations"} */}
+        Locations
       </MenuButton>
       <MenuList>
         {asdasd?.locations?.results.map((platform) => (
